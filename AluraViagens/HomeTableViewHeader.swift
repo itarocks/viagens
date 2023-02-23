296f7275db5903c9cc9ee8aa6aa15b1f678e3cfd
Itamar <itamarrochaa@yahoo.com>
2023-02-23 14:46:35 -0300

    adicionando constraint no header

---
+++ AluraViagens/HomeTableViewHeader.swift
@@ -0,0 +1,26 @@
+//
+//  HomeTableViewHeader.swift
+//  AluraViagens
+//
+//  Created by Itamar Rocha on 23/02/23.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    @IBOutlet weak var tituloLabel: UILabel!
+    
+    
+    @IBOutlet weak var headerView: UIView!
+    
+    @IBOutlet weak var bannerImageView: UIImageView!
+}
